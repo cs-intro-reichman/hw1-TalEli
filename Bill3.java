cb34cbebbeb897e5471a1ae32d7c2d6f44e3a47b
tal <tal.ariel@runi.post.ac.il>
2024-11-13 23:56:08 +0200

    Bill 3 changed

--- Bill3.java
+++ Bill3.java
@@ -5,13 +5,13 @@ public class Bill3 {
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		int sum = Integer.parseInt(args[3]) ;
-		
+		int sum = Integer.parseInt(args[3]);
+
 		double finalSum = sum/3.0;
 
 		finalSum= Math.ceil(finalSum);
-		System.out.print(" Dear "+ name1+" " + name2 +" "+ name3 + ", pay " + finalSum + " Shekels each" );
-
+		System.out.print("Dear "+ name1+", " + name2 +", and "+ name3 + ": pay " + finalSum + " Shekels each." );
+//Dear Dan, Lisa, and Ron: pay 34.0 Shekels each.
 	}
 }
 
